cdd2d7b34ede79c0c785277ad7d4247f5a844764
cetracy <156886202+cetracy@users.noreply.github.com>
2024-01-18 13:49:35 +0000

    new changes

---
+++ script.js
@@ -0,0 +1,9 @@
+function openModal(imageSrc) {
+    document.getElementById('myModal').style.display = 'block';
+    document.getElementById('modalImg').src = imageSrc;
+  }
+  
+  function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+  }
+  
